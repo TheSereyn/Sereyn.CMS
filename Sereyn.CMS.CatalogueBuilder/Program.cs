fa5d9ef5094d8207ed71738ae6a324435c51c344
Lee Buxton <buxton50@hotmail.com>
2020-07-27 12:53:31 +0100

    Added in a empty Blazor WebAssembly blog style example project. Added a console application to be used to build catalogue files.

---
+++ Sereyn.CMS.CatalogueBuilder/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Sereyn.CMS.CatalogueBuilder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
